572c8afefcb1b02dce63a82db70fa312aa5d8bee
OdoiOkoth <odoiokoth@gmail.com>
2022-10-10 07:09:13 -0700

    1-init_dog.c Task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,7 @@
 *age: second member
 *owner: third member
 *
-*Description: longer description
+*Description: struct called "dog" that stores its name, its age and the name of its owner.
 */
 struct dog
 {
@@ -19,13 +19,13 @@ struct dog
 /**
 *dog_t -typedef for struct dog
 */
-typedef struct dog dog _t;
+typedef struct dog dog_t;
 
-void init - dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
-int - strlen(char *s);
+int *_strlen(char *s);
 
 #endif
